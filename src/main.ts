78bad3bac358e9d6d68fb9bbc1609c0e80161dbb
Piyush Dhanwal <piyushdhanwal2003@gmail.com>
2025-10-07 15:04:34 +0530

    inprogress connection

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,20 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-    
+
+  // Serve all routes under the global API prefix
+  app.setGlobalPrefix('api');
+
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://192.168.0.188:5173'],
-    
+    origin: [
+      'http://localhost:5173',
+      'http://192.168.0.188:5173',
+      'https://gurukulbakery.com',
+      'http://gurukulbakery.com',
+    ],
     credentials: true, // optional: use if you're dealing with cookies/session
   });
-  
+
   await app.listen(process.env.PORT ?? 3001);
   console.log(`🚀 App running on port ${process.env.PORT}`);
 }
